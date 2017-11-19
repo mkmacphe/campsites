ae4f88635e29a8cc70eb9dbda35e647799df3f7f
Mary Kate <mkmacphe@edu.uwaterloo.ca>
2017-11-19 12:45:20 -0500

    Added functions for when button is pressed

--- src/nottree.java
+++ src/nottree.java
@@ -1,3 +1,7 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+
 public class nottree extends tile {
     private labeltypes guess = labeltypes.EMPTY;
     public nottree(labeltypes tentOrDirt) {
@@ -9,5 +13,38 @@ public class nottree extends tile {
     public labeltypes getGuess(){
         return(this.guess);
     }
+    public void setTent(){
+        if(guess != labeltypes.TENT){
+            makeGuess(labeltypes.TENT);
+            try {
+                Image img = ImageIO.read(getClass().getResource("resources/tent.png"));
+                setIcon(new ImageIcon(img));
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+        else{
+            makeGuess(labeltypes.EMPTY);
+            setIcon(null);
+        }
+    }
+
+    public void setDirt(){
+        if(guess != labeltypes.DIRT){
+            makeGuess(labeltypes.DIRT);
+            try {
+                Image img = ImageIO.read(getClass().getResource("resources/dirt.png"));
+                setIcon(new ImageIcon(img));
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+        else{
+            makeGuess(labeltypes.EMPTY);
+            setIcon(null);
+        }
+    }
+
+    
 }
 
--- src/tree.java
+++ src/tree.java
@@ -1,6 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.*;
+
 public class tree extends tile {
     public tree(){
         super(labeltypes.TREE);
-        setText("Tree");
+        try {
+            Image img = ImageIO.read(getClass().getResource("resources/tree.png"));
+            setIcon(new ImageIcon(img));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 }
